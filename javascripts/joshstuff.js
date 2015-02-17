126533d4f09ee5dc4faafb64f30f0bc951932295
Joshua Eichler-Summers <todo.eichler.summers@gmail.com>
2015-02-17 23:19:15 +0000

    mobile improvements for twitter

--- javascripts/joshstuff.js
+++ javascripts/joshstuff.js
@@ -1,1 +1,8 @@
-joshstuff.js
+$(document).ready(function() {
+	if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
+	    $('.mobiletwitterbutton').css('display', 'block');
+	    $('.share-links').css('display', 'none');
+	} else {
+		$('.mobiletwitterbutton').css('display', 'none');
+	}
+});
