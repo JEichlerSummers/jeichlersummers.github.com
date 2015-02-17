ce0021ae8101813a98d204611edcc53f1f727755
Joshua Eichler-Summers <todo.eichler.summers@gmail.com>
2015-02-17 23:33:03 +0000

    making things good on mobile

--- javascripts/joshstuff.js
+++ javascripts/joshstuff.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	if (navigator.userAgent.match(/(Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini)/)) {
 	    $('.mobiletwitterbutton').css('display', 'block');
-	    //$('.share-links').css('display', 'none');
+	    $('.share-links').css('display', 'none');
 	} else {
 		$('.mobiletwitterbutton').css('display', 'none');
 	}
